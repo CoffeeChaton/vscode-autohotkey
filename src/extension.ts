9bb13888f2c99f077fe72ca9b45e302888bfcc0a
CoffeeChaton <63182963+CoffeeChaton@users.noreply.github.com>
2020-04-05 08:53:56 +0000

    Remove Run/Compile、 Debug Script
      Because of compilation warn && i don't need it.

--- src/extension.ts
+++ src/extension.ts
@@ -7,7 +7,10 @@ import { FileProvider } from './provider/FileProvider';
 import { FormatProvider } from './provider/FormatProvider';
 import SymBolProvider from './provider/SymbolProvider';
 
-
+/*
+  Remove Run/Compile、 Debug Script
+  Because of compilation warn && i don't need it.
+*/
 // eslint-disable-next-line import/prefer-default-export
 export function activate(context: vscode.ExtensionContext) {
   const language = { language: 'ahk' };
