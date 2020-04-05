6662f76ef7685ca242e48a4207cf4e95a03b95df
CoffeeChaton <63182963+CoffeeChaton@users.noreply.github.com>
2020-04-05 07:38:03 +0000

    Update out.ts

--- src/common/out.ts
+++ src/common/out.ts
@@ -1,15 +1,20 @@
-"user strict";
-import * as vscode from "vscode";
-import { OutputChannel } from "vscode";
+/* eslint-disable import/prefer-default-export */
+/* eslint-disable import/no-unresolved */
+/* eslint-disable no-unused-vars */
+
+// 'user strict';
+
+import * as vscode from 'vscode';
+import { OutputChannel } from 'vscode';
 
 export class Out {
-    public static log(value: Object) {
-        if (!this.channel) {
-            this.channel = vscode.window.createOutputChannel("AHK");
-        }
-        this.channel.show(true);
-        this.channel.appendLine(value + "");
+  public static log(value: Object) {
+    if (!this.channel) {
+      this.channel = vscode.window.createOutputChannel('AHK');
     }
+    this.channel.show(true);
+    this.channel.appendLine(`${value}`);
+  }
 
-    private static channel: OutputChannel;
+  private static channel: OutputChannel;
 }
