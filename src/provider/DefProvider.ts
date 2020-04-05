3e337096370edaa1d3a47a2c8c1ee68c3aeec025
CoffeeChaton <63182963+CoffeeChaton@users.noreply.github.com>
2020-04-05 07:33:24 +0000

    Update DefProvider.ts

--- src/provider/DefProvider.ts
+++ src/provider/DefProvider.ts
@@ -1,55 +1,74 @@
-import * as vscode from "vscode";
-import { Detecter } from "../core/Detecter";
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/extensions */
+/* eslint-disable no-restricted-syntax */
+// eslint-disable-next-line import/no-unresolved
+import * as vscode from 'vscode';
+import { Detecter } from '../core/Detecter';
 
-export class DefProvider implements vscode.DefinitionProvider {
-
-    public async provideDefinition(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken): Promise<vscode.Location | vscode.Location[] | vscode.LocationLink[]> {
-
-        const fileLink = await this.tryGetFileLink(document, position);
-        if (fileLink) {
-            return fileLink;
-        }
-        const methodLink = await this.tryGetMethodLink(document, position);
-        if (methodLink) {
-            return methodLink;
-        }
-
-        return null;
-
+export default class DefProvider implements vscode.DefinitionProvider {
+  public async provideDefinition(document: vscode.TextDocument,
+    position: vscode.Position,
+    // eslint-disable-next-line no-unused-vars
+    token: vscode.CancellationToken)
+    : Promise<vscode.Location | vscode.Location[] | vscode.LocationLink[] | null> {
+    const fileLink = await this.tryGetFileLink(document, position);
+    if (fileLink) {
+      return fileLink;
+    }
+    const methodLink = await this.tryGetMethodLink(document, position);
+    if (methodLink) {
+      return methodLink;
     }
 
-    public async tryGetFileLink(document: vscode.TextDocument, position: vscode.Position) {
-        const { text } = document.lineAt(position.line);
-        const includeMatch = text.match(/(?<=#include).+?\.(ahk|ext)\b/i);
-        if (includeMatch) {
-            const parent = document.uri.path.substr(0, document.uri.path.lastIndexOf("/"));
-            return new vscode.Location(vscode.Uri.file(includeMatch[0].trim().replace(/(%A_ScriptDir%|%A_WorkingDir%)/, parent)), new vscode.Position(0, 0));
-        }
+    return null;
+  }
 
+  // eslint-disable-next-line class-methods-use-this
+  public async tryGetFileLink(document: vscode.TextDocument, position: vscode.Position)
+    : Promise<vscode.Location | null> {
+    const { text } = document.lineAt(position.line);
+    const includeMatch = text.match(/(?<=#include).+?\.(ahk|ext)\b/i);
+    if (includeMatch) {
+      const substrZero = 0;
+      const parent = document.uri.path.substr(substrZero, document.uri.path.lastIndexOf('/'));
+      const uri = vscode.Uri.file(includeMatch[0].trim().replace(/(%A_ScriptDir%|%A_WorkingDir%)/,
+        parent));
+      const PositionZero = 0;
+      return new vscode.Location(uri,
+        new vscode.Position(PositionZero, PositionZero));
     }
+    return null;
+  }
 
-    public async tryGetMethodLink(document: vscode.TextDocument, position: vscode.Position) {
-
-        const { text } = document.lineAt(position.line);
-        const word = document.getText(document.getWordRangeAtPosition(position));
-        const callReg = new RegExp("\\b" + word + "\\s*\\(.*?\\)");
-        if (!callReg.exec(text)) {
-            return;
-        }
-        for (const method of await Detecter.getMethodList(document)) {
-            if (method.name.indexOf(word) !== -1) {
-                return new vscode.Location(document.uri, new vscode.Position(method.line, document.lineAt(method.line).text.indexOf(word)));
-            }
-        }
-        for (const filePath of Detecter.getCacheFile()) {
-            const tempDocument = await vscode.workspace.openTextDocument(filePath);
-            for (const method of await Detecter.getMethodList(tempDocument)) {
-                if (method.name.indexOf(word) !== -1) {
-                    return new vscode.Location(tempDocument.uri, new vscode.Position(method.line, tempDocument.lineAt(method.line).text.indexOf(word)));
-                }
-            }
-        }
-
+  // eslint-disable-next-line class-methods-use-this
+  public async tryGetMethodLink(document: vscode.TextDocument, position: vscode.Position)
+    : Promise<vscode.Location | null> {
+    const indexOfZero = -1;
+    const { text } = document.lineAt(position.line);
+    const word = document.getText(document.getWordRangeAtPosition(position));
+    const callReg = new RegExp(`\\b${word}\\s*\\(.*?\\)`);
+    if (!callReg.exec(text)) {
+      return null;
     }
-
+    for (const method of await Detecter.getFuncList(document)) {
+      if (method.name.indexOf(word) !== indexOfZero) {
+        // eslint-disable-next-line consistent-return
+        return new vscode.Location(document.uri,
+          new vscode.Position(method.line, document.lineAt(method.line).text.indexOf(word)));
+      }
+    }
+    for (const filePath of Detecter.getCacheFile()) {
+      // eslint-disable-next-line no-await-in-loop
+      const tempDocument = await vscode.workspace.openTextDocument(filePath);
+      // eslint-disable-next-line no-await-in-loop
+      for (const method of await Detecter.getFuncList(tempDocument)) {
+        if (method.name.indexOf(word) !== indexOfZero) {
+          // eslint-disable-next-line consistent-return
+          return new vscode.Location(tempDocument.uri,
+            new vscode.Position(method.line, tempDocument.lineAt(method.line).text.indexOf(word)));
+        }
+      }
+    }
+    return null;
+  }
 }
