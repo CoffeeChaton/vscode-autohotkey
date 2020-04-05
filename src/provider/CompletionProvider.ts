cd0fbdbdd8ce7f8b7748ae236e182c6fd56f0e28
CoffeeChaton <63182963+CoffeeChaton@users.noreply.github.com>
2020-04-05 07:33:51 +0000

    Update CompletionProvider.ts

--- src/provider/CompletionProvider.ts
+++ src/provider/CompletionProvider.ts
@@ -1,38 +1,43 @@
-import * as vscode from "vscode";
-import { Detecter } from "../core/Detecter";
+/* eslint-disable no-unused-vars */
+/* eslint-disable class-methods-use-this */
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-unresolved */
+import * as vscode from 'vscode';
+import { Detecter } from '../core/Detecter';
 
 
+// eslint-disable-next-line import/prefer-default-export
 export class CompletionProvider implements vscode.CompletionItemProvider {
+  private keywordList: string[] = [];
 
-    private keywordList: string[] = [];
-    private keywordComplectionItems: vscode.CompletionItem[] = [];
-    constructor() {
-        this.initKeywordComplectionItem();
-    }
+  private keywordComplectionItems: vscode.CompletionItem[] = [];
 
-    public async provideCompletionItems(document: vscode.TextDocument, position: vscode.Position): Promise<vscode.CompletionItem[] | vscode.CompletionList> {
+  constructor() {
+    this.initKeywordComplectionItem();
+  }
 
-        const result: vscode.CompletionItem[] = [];
-        (await Detecter.getMethodList(document, true)).forEach((method) => {
-            const completionItem = new vscode.CompletionItem(method.name + "()", vscode.CompletionItemKind.Method);
-            completionItem.detail = method.comnent;
-            result.push(completionItem);
-        });
+  public async provideCompletionItems(document: vscode.TextDocument,
+    position: vscode.Position): Promise<vscode.CompletionItem[] | vscode.CompletionList> {
+    const result: vscode.CompletionItem[] = [];
+    (await Detecter.getFuncList(document, true)).forEach((method) => {
+      const completionItem = new vscode.CompletionItem(`${method.name}()`, vscode.CompletionItemKind.Method);
+      completionItem.detail = method.comnent;
+      result.push(completionItem);
+    });
 
-        return this.keywordComplectionItems.concat(result);
-    }
+    return this.keywordComplectionItems.concat(result);
+  }
 
-    public resolveCompletionItem?(item: vscode.CompletionItem): vscode.ProviderResult<vscode.CompletionItem> {
-        return item;
-    }
+  public resolveCompletionItem?(item: vscode.CompletionItem)
+    : vscode.ProviderResult<vscode.CompletionItem> {
+    return item;
+  }
 
-    private initKeywordComplectionItem() {
-        this.keywordList.forEach((keyword) => {
-            const keywordComplectionItem = new vscode.CompletionItem(keyword + " ");
-            keywordComplectionItem.kind = vscode.CompletionItemKind.Property;
-            this.keywordComplectionItems.push(keywordComplectionItem);
-        });
-    }
-
+  private initKeywordComplectionItem() {
+    this.keywordList.forEach((keyword) => {
+      const keywordComplectionItem = new vscode.CompletionItem(`${keyword} `);
+      keywordComplectionItem.kind = vscode.CompletionItemKind.Property;
+      this.keywordComplectionItems.push(keywordComplectionItem);
+    });
+  }
 }
-
