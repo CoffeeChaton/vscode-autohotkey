33e3812d823ef5b86953b24f636006daa94d69f2
CoffeeChaton <63182963+CoffeeChaton@users.noreply.github.com>
2020-04-05 07:37:38 +0000

    Update setting.ts

--- src/common/setting.ts
+++ src/common/setting.ts
@@ -1,31 +1,32 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable no-unused-vars */
+/* eslint-disable import/prefer-default-export */
 import * as vscode from 'vscode';
 import * as fs from 'fs';
 
 export class Setting {
-    executePath: string;
-    private settingPath: string;
-    private interSetting = {};
-    constructor(private context: vscode.ExtensionContext) {
-        let extPath = this.context['globalStoragePath'];
-        if (!fs.existsSync(extPath)) {
-            fs.mkdirSync(extPath)
-        }
-        this.settingPath = extPath + '/setting.json'
+  executePath: string;
 
+  private settingPath: string;
+
+  constructor(private context: vscode.ExtensionContext) {
+    const extPath = this.context.globalStoragePath;
+    if (!fs.existsSync(extPath)) {
+      fs.mkdirSync(extPath);
     }
-    public get(key: string): string {
-        if (!fs.existsSync(this.settingPath)) return this.interSetting[key];
-        try {
-            return JSON.parse(fs.readFileSync(this.settingPath, "utf8"))[key];
-        } catch (err) {
-            return this.interSetting[key];
-        }
+    this.settingPath = `${extPath}/setting.json`;
+  }
+
+  public get(key: string): string | null {
+    if (!fs.existsSync(this.settingPath)) return null;
+    try {
+      return JSON.parse(fs.readFileSync(this.settingPath, 'utf8'))[key];
+    } catch (err) {
+      return null;
     }
-    public set(key: string, value: string) {
-        try {
-            fs.writeFileSync(this.settingPath, JSON.stringify({ [key]: value }))
-        } catch (err) {
-            this.interSetting[key] = value;
-        }
-    }
+  }
+
+  public set(key: string, value: string) {
+    fs.writeFileSync(this.settingPath, JSON.stringify({ [key]: value }));
+  }
 }
