e852b92b7d4db8333dee23c68c4a929571d7657a
CoffeeChaton <63182963+CoffeeChaton@users.noreply.github.com>
2022-05-09 21:15:02 +0800

    feat: diag Swtich

---
+++ src/provider/Diagnostic/tools/getSwErr.ts
@@ -0,0 +1,63 @@
+/* eslint no-magic-numbers: ["error", { "ignore": [-1,0,1,20] }] */
+import * as vscode from 'vscode';
+import { CAhkCase, CAhkDefault, CAhkSwitch } from '../../../AhkSymbol/CAhkSwitch';
+import { TAhkSymbol } from '../../../AhkSymbol/TAhkSymbolIn';
+import { EDiagCode } from '../../../diag';
+import { setDiagnostic } from './setDiagnostic';
+
+function getDefaultNumber(swChildren: (CAhkCase | CAhkDefault)[]): number {
+    let iDefault = 0;
+    for (const e of swChildren) {
+        if (e instanceof CAhkDefault) iDefault++;
+    }
+    return iDefault;
+}
+
+function getCaseNumber(swChildren: (CAhkCase | CAhkDefault)[]): number {
+    let iCase = 0;
+    for (const e of swChildren) {
+        if (e instanceof CAhkCase) iCase++;
+    }
+    return iCase;
+}
+
+function setErrDefault(sw: CAhkSwitch): null | vscode.Diagnostic {
+    const iDefault: number = getDefaultNumber(sw.children);
+
+    if (iDefault === 1) return null; // OK
+
+    return iDefault === 0
+        ? setDiagnostic(EDiagCode.code110, sw.range, vscode.DiagnosticSeverity.Warning, []) // not find
+        : setDiagnostic(EDiagCode.code111, sw.range, vscode.DiagnosticSeverity.Warning, []); // too Much
+}
+
+function setErrCase(sw: CAhkSwitch): null | vscode.Diagnostic {
+    const iCase: number = getCaseNumber(sw.children);
+
+    // too Much
+    if (iCase >= 20) return setDiagnostic(EDiagCode.code112, sw.range, vscode.DiagnosticSeverity.Warning, []);
+
+    return iCase === 0 // not find
+        ? setDiagnostic(EDiagCode.code113, sw.range, vscode.DiagnosticSeverity.Warning, [])
+        : null; // at 1~19
+}
+
+function setErrSwNameNotFind(sw: CAhkSwitch): null | vscode.Diagnostic {
+    return sw.name === ''
+        ? setDiagnostic(EDiagCode.code114, sw.range, vscode.DiagnosticSeverity.Information, [])
+        : null;
+}
+
+export function getSwErr(sw: TAhkSymbol, displayErr: readonly boolean[]): vscode.Diagnostic[] {
+    const digS: vscode.Diagnostic[] = [];
+    if (
+        sw instanceof CAhkSwitch
+        && displayErr[sw.range.start.line]
+    ) {
+        for (const fn of [setErrDefault, setErrCase, setErrSwNameNotFind]) {
+            const err: vscode.Diagnostic | null = fn(sw);
+            if (err !== null) digS.push(err);
+        }
+    }
+    return digS;
+}
--- src/provider/Diagnostic/tools/getTreeErr.ts
+++ src/provider/Diagnostic/tools/getTreeErr.ts
@@ -1,101 +1,14 @@
-/* eslint no-magic-numbers: ["error", { "ignore": [-1,0,1,20] }] */
 import * as vscode from 'vscode';
 import { TAhkSymbol } from '../../../AhkSymbol/TAhkSymbolIn';
-import { EDiagCode } from '../../../diag';
-import { setDiagnostic } from './setDiagnostic';
-
-function getDefaultNumber(swChildren: Readonly<TAhkSymbol[]>): number {
-    let iDefault = 0;
-    swChildren.forEach((e) => {
-        if (e.name === 'Default :') iDefault++;
-    });
-    return iDefault;
-}
-
-function getCaseNumber(swCh: Readonly<TAhkSymbol[]>): number {
-    let iCase = 0;
-    swCh.forEach((e) => {
-        if (e.name.startsWith('Case ')) iCase++;
-    });
-    return iCase;
-}
-
-function setErrDefaultNotFind(sw: TAhkSymbol): vscode.Diagnostic {
-    const value = EDiagCode.code110;
-    const range = sw.selectionRange;
-    return setDiagnostic(value, range, vscode.DiagnosticSeverity.Information, []);
-}
-
-function setErrDefaultTooMuch(sw: TAhkSymbol): vscode.Diagnostic {
-    const value = EDiagCode.code111;
-    const { range } = sw;
-    return setDiagnostic(value, range, vscode.DiagnosticSeverity.Information, []);
-}
-
-function setErrDefault(sw: TAhkSymbol): null | vscode.Diagnostic {
-    const iDefault = getDefaultNumber(sw.children);
-    switch (iDefault) {
-        case 0:
-            return setErrDefaultNotFind(sw);
-        case 1:
-            return null;
-        default:
-            return setErrDefaultTooMuch(sw);
-    }
-}
-
-function setCaseTooMuch(sw: TAhkSymbol): vscode.Diagnostic {
-    const value = EDiagCode.code112;
-    const { range } = sw;
-    return setDiagnostic(value, range, vscode.DiagnosticSeverity.Information, []);
-}
-
-function setErrCaseZero(sw: TAhkSymbol): vscode.Diagnostic {
-    const value = EDiagCode.code113;
-    const { range } = sw;
-    return setDiagnostic(value, range, vscode.DiagnosticSeverity.Information, []);
-}
-
-function setErrCase(sw: TAhkSymbol): null | vscode.Diagnostic {
-    const iCase = getCaseNumber(sw.children);
-    switch (true) {
-        case iCase < 20 && iCase > 0:
-            return null;
-        case iCase >= 20:
-            return setCaseTooMuch(sw);
-        case iCase < 1:
-            return setErrCaseZero(sw);
-        default:
-            return null;
-    }
-}
-
-function setErrSwNameNotFind(sw: TAhkSymbol): null | vscode.Diagnostic {
-    if (!sw.name.startsWith('!!')) return null;
-    const value = EDiagCode.code114;
-    const { range } = sw;
-    return setDiagnostic(value, range, vscode.DiagnosticSeverity.Error, []);
-}
-
-function getSwErr(sw: TAhkSymbol, displayErr: readonly boolean[]): vscode.Diagnostic[] {
-    const digS: vscode.Diagnostic[] = [];
-    if (
-        sw.kind === vscode.SymbolKind.Enum
-        && displayErr[sw.range.start.line]
-        && sw.detail === 'Switch'
-    ) {
-        [setErrDefault, setErrCase, setErrSwNameNotFind].forEach((fn) => {
-            const err: vscode.Diagnostic | null = fn(sw);
-            if (err !== null) digS.push(err);
-        });
-    }
-    return digS;
-}
+import { getSwErr } from './getSwErr';
 
 export function getTreeErr(children: readonly TAhkSymbol[], displayErr: readonly boolean[]): vscode.Diagnostic[] {
     const digS: vscode.Diagnostic[] = [];
-    children.forEach((ch) => {
-        digS.push(...getSwErr(ch, displayErr), ...getTreeErr(ch.children, displayErr));
-    });
+    for (const ch of children) {
+        digS.push(
+            ...getSwErr(ch, displayErr),
+            ...getTreeErr(ch.children, displayErr),
+        );
+    }
     return digS;
 }
