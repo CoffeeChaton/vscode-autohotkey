c1fbdf11ba32d40f073b3306b74885ca6584fd83
CoffeeChaton <63182963+CoffeeChaton@users.noreply.github.com>
2022-09-27 01:51:35 +0800

    add:TODO list

--- src/tools/DeepAnalysis/FnVar/getFnVarDef.ts
+++ src/tools/DeepAnalysis/FnVar/getFnVarDef.ts
@@ -44,6 +44,20 @@ export function getFnVarDef(
         forLoop(need); // for var1 , var2 in
         OutputVarCommandBase(need, fistWordUp);
         OutputVarCommandPlus(need, fistWordUp);
+        // TODO: Catch , OutputVar
+
+        // if i have time... maybe?
+        //
+        // LV_GetText(OutputVar,
+        // TV_GetText(OutputVar, ItemID)
+
+        // not plan
+        // DllCall("DllFile\Function" , Type1, Arg1, Type2, Arg2, "Cdecl ReturnType")
+        // ----------------------------------------------------------------^
+        // FoundPos := RegExMatch(Haystack, NeedleRegEx , OutputVar, StartingPos := 1)
+        // --------------------------------------------------^
+        // New GUI options: +HwndOutputVar, +ParentGUI
+        //                     ^ ...WTF?
     }
 
     return valMap;
