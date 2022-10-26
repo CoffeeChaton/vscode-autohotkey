cccba900b7f20381a273b2dd1e4929f4999837ad
CoffeeChaton <63182963+CoffeeChaton@users.noreply.github.com>
2022-10-26 22:20:25 +0800

    feat: allow exceptions of `code107`

--- src/provider/Diagnostic/tools/lineErr/assignErr.ts
+++ src/provider/Diagnostic/tools/lineErr/assignErr.ts
@@ -1,20 +1,27 @@
 import * as vscode from 'vscode';
 import { EDiagCode } from '../../../../diag';
-import type { TAhkTokenLine } from '../../../../globalEnum';
-import { EDetail } from '../../../../globalEnum';
+import type { TAhkTokenLine, TTokenStream } from '../../../../globalEnum';
+import { EDetail, EMultiline } from '../../../../globalEnum';
+
 import { CDiagBase } from '../CDiagBase';
 
 export function assignErr({
     detail,
     line,
     lStr,
-}: TAhkTokenLine): CDiagBase | null {
-    return detail.includes(EDetail.inSkipSign2)
-        ? new CDiagBase({
-            value: EDiagCode.code107,
-            range: new vscode.Range(line, lStr.search(/\S/u), line, lStr.indexOf('=')),
-            severity: vscode.DiagnosticSeverity.Warning,
-            tags: [],
-        })
-        : null; // 0
+}: TAhkTokenLine, DocStrMap: TTokenStream): CDiagBase | null {
+    if (!detail.includes(EDetail.inSkipSign2)) return null;
+
+    const tokenNext: TAhkTokenLine | undefined = DocStrMap[line + 1] as TAhkTokenLine | undefined;
+
+    if (tokenNext !== undefined && tokenNext.multiline === EMultiline.start) {
+        return null;
+    }
+
+    return new CDiagBase({
+        value: EDiagCode.code107,
+        range: new vscode.Range(line, lStr.search(/\S/u), line, lStr.indexOf('=')),
+        severity: vscode.DiagnosticSeverity.Warning,
+        tags: [],
+    });
 }
