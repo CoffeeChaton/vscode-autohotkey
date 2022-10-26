470a8392a093dda204c4f309e4f9cf40c28b8d0d
CoffeeChaton <63182963+CoffeeChaton@users.noreply.github.com>
2022-10-26 22:21:16 +0800

    feat: allow exceptions of `code107` part2

--- src/provider/Diagnostic/tools/getLineErr.ts
+++ src/provider/Diagnostic/tools/getLineErr.ts
@@ -8,10 +8,13 @@ export function getLineErr(DocStrMap: TTokenStream): CDiagBase[] {
     const errList: CDiagBase[] = [];
 
     type TFn = (params: TAhkTokenLine) => CDiagBase | null;
-    const fnList: TFn[] = [assignErr, getObjBaseErr, getCommandErr];
+    const fnList: TFn[] = [getObjBaseErr, getCommandErr];
 
     for (const token of DocStrMap) {
         if (!token.displayErr) continue;
+
+        const ed1: CDiagBase | null = assignErr(token, DocStrMap);
+        if (ed1 !== null) errList.push(ed1);
 
         for (const fn of fnList) {
             const ed: CDiagBase | null = fn(token);
