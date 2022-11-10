de7cadd8fa61b2876f4a68088bf420a661cb682b
CoffeeChaton <63182963+CoffeeChaton@users.noreply.github.com>
2022-11-10 22:40:26 +0800

    refactor: initialization-only function

--- src/tools/Built-in/statement_vsc.ts
+++ src/tools/Built-in/statement_vsc.ts
@@ -5,30 +5,30 @@ import * as vscode from 'vscode';
 import type { TStatementElement } from './statement';
 import { Statement } from './statement';
 
-function Statement2Md(element: TStatementElement): vscode.MarkdownString {
-    const {
-        keyRawName,
-        doc,
-        link,
-        exp,
-    } = element;
-    const md: vscode.MarkdownString = new vscode.MarkdownString('', true)
-        .appendCodeblock(keyRawName, 'ahk')
-        .appendMarkdown(doc)
-        .appendMarkdown('\n')
-        .appendMarkdown(`[(Read Doc)](${link})`)
-        .appendMarkdown('\n\n***')
-        .appendMarkdown('\n\n*exp:*')
-        .appendCodeblock(exp.join('\n'), 'ahk');
-
-    md.supportHtml = true;
-    return md;
-}
-
 type TStatementMDMap = ReadonlyMap<string, vscode.MarkdownString>;
 type TSnippetStatement = readonly vscode.CompletionItem[];
 
 export const [StatementMDMap, snippetStatement] = ((): [TStatementMDMap, TSnippetStatement] => {
+    function Statement2Md(element: TStatementElement): vscode.MarkdownString {
+        const {
+            keyRawName,
+            doc,
+            link,
+            exp,
+        } = element;
+        const md: vscode.MarkdownString = new vscode.MarkdownString('', true)
+            .appendCodeblock(keyRawName, 'ahk')
+            .appendMarkdown(doc)
+            .appendMarkdown('\n')
+            .appendMarkdown(`[(Read Doc)](${link})`)
+            .appendMarkdown('\n\n***')
+            .appendMarkdown('\n\n*exp:*')
+            .appendCodeblock(exp.join('\n'), 'ahk');
+
+        md.supportHtml = true;
+        return md;
+    }
+
     const map1: Map<string, vscode.MarkdownString> = new Map<string, vscode.MarkdownString>();
     const List2: vscode.CompletionItem[] = [];
     for (const [k, v] of Object.entries(Statement)) {
