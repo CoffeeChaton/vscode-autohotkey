17ef859b6b07e97bad8e2b2168d6a219aaea019a
CoffeeChaton <63182963+CoffeeChaton@users.noreply.github.com>
2022-11-20 14:48:26 +0800

    fix: rm diag some config

--- src/diag.ts
+++ src/diag.ts
@@ -105,10 +105,9 @@ export const enum EDiagCode {
     code902 = 902,
     code903 = 903,
     code904 = 904,
-
-    // of EDiagDeep
-    code908 = 908, // {{{ of one Line
-    code909 = 909, // }}} of one Line
+    // // of EDiagDeep
+    // code908 = 908, // {{{ of one Line
+    // code909 = 909, // }}} of one Line
 }
 
 type TLink =
@@ -118,8 +117,6 @@ export type TDiags = {
     [k in EDiagCode]: {
         msg: string;
         path: TLink;
-        Deprecated?: true; // FIXME: add config option to pack.json
-        notDoc?: true; // FIXME: add config option to pack.json
         // FIXME let  pack.json set diag value as
         // 0 1 2
         // none info warning
@@ -130,13 +127,10 @@ export const Diags: DeepReadonly<TDiags> = {
     107: {
         msg: 'assign warning',
         path: 'https://github.com/CoffeeChaton/vscode-autohotkey-NekoHelp/tree/master/note#diag107',
-        notDoc: true,
-        Deprecated: true,
     },
     111: {
         msg: 'Default : not find ',
         path: 'https://www.autohotkey.com/docs/commands/Switch.htm',
-        notDoc: true,
     },
     112: {
         msg: 'Case : > 20',
@@ -149,12 +143,10 @@ export const Diags: DeepReadonly<TDiags> = {
     114: {
         msg: 'Switch name not find',
         path: 'https://www.autohotkey.com/docs/commands/Switch.htm',
-        notDoc: true,
     },
     120: {
         msg: 'unknown options of Multiline',
         path: 'https://www.autohotkey.com/docs/Scripts.htm#Join',
-        notDoc: true,
     },
     121: {
         msg: 'Multiline:join > 15 characters',
@@ -163,7 +155,6 @@ export const Diags: DeepReadonly<TDiags> = {
     122: {
         msg: 'ahk-neko-help not supported (, `) flag now.',
         path: 'https://github.com/CoffeeChaton/vscode-autohotkey-NekoHelp/tree/master/note#diag122',
-        notDoc: true,
     },
     124: {
         msg: '`"` is not closed',
@@ -201,105 +192,89 @@ export const Diags: DeepReadonly<TDiags> = {
     700: {
         msg: 'try to use function replace Command(obsolete code)',
         path: 'https://www.autohotkey.com/docs/Language.htm#commands-vs-functions',
-        notDoc: true,
+        // TODO -> move more 700 -> 8xx code
     },
     801: {
         msg: 'Use `Loop, Reg, KeyName` instead.',
         path: 'https://www.autohotkey.com/docs/commands/LoopReg.htm#old',
-        Deprecated: true,
     },
     802: {
         msg: 'Use `Loop, Files, FilePattern` instead.',
         path: 'https://www.autohotkey.com/docs/commands/LoopFile.htm#old',
-        Deprecated: true,
     },
     803: {
         msg: 'Use `Var := Var / Value` or `Var /= Value` instead.',
         path: 'https://www.autohotkey.com/docs/commands/EnvDiv.htm',
-        Deprecated: true,
     },
     804: {
         msg: 'Use `Var := Var * Value` or `Var *= Value` instead.',
         path: 'https://www.autohotkey.com/docs/commands/EnvMult.htm',
-        Deprecated: true,
     },
     806: {
         msg: 'Use `If (expression)` instead.',
         path: 'https://www.autohotkey.com/docs/commands/IfEqual.htm',
-        Deprecated: true,
     },
     811: {
         msg: 'Use the `OnClipboardChange()` function instead.',
         path: 'https://www.autohotkey.com/docs/commands/OnClipboardChange.htm#label',
-        Deprecated: true,
     },
     812: {
         msg: 'Use the `OnExit()` function instead.',
         path: 'https://www.autohotkey.com/docs/commands/OnExit.htm#command',
-        Deprecated: true,
     },
     813: {
         msg: 'Use the `Gui,` command instead.',
         path: 'https://www.autohotkey.com/docs/commands/Progress.htm',
-        Deprecated: true,
     },
     814: {
         msg: 'Use expression assignments like `Var := Value` instead.',
         path: 'https://www.autohotkey.com/docs/commands/SetEnv.htm',
-        Deprecated: true,
     },
     815: {
         msg: 'Use the `Format()` function instead.',
         path: 'https://www.autohotkey.com/docs/commands/SetFormat.htm',
-        Deprecated: true,
     },
     816: {
         msg: 'Use the `Gui` command instead.',
         path: 'https://www.autohotkey.com/docs/commands/SplashTextOn.htm',
-        Deprecated: true,
     },
     824: {
         msg: 'Deprecated: This command is not recommended for use in new scripts.',
         path: 'https://www.autohotkey.com/docs/commands/Transform.htm',
-        Deprecated: true,
         // TODO search SubCommand and suggest of new func https://www.autohotkey.com/docs/commands/Transform.htm
     },
     825: {
         msg: 'Deprecated: #AllowSameLineComments was removed.',
         path: 'https://www.autohotkey.com/docs/commands/_AllowSameLineComments.htm',
-        Deprecated: true,
     },
     901: {
         msg: 'ahk-doc not recommended and ahk-neko-help is not work of this Directives.',
         path: 'https://www.autohotkey.com/docs/commands/_EscapeChar.htm',
-        Deprecated: true,
     },
     902: {
-        msg: 'ahk-doc not recommended and ahk-neko-help is not work of this Directives.',
+        msg: 'ahk-doc not recommended and`ahk-neko-help is not work of this Directives.',
         path: 'https://www.autohotkey.com/docs/commands/_CommentFlag.htm',
-        Deprecated: true,
     },
     903: {
         msg: 'ahk-doc not recommended and ahk-neko-help is not work of this Directives.',
         path: 'https://www.autohotkey.com/docs/commands/_EscapeChar.htm#Related',
-        Deprecated: true,
     },
     904: {
-        msg: 'ahk-neko-help is not work of this Directives',
+        msg: 'ahk-neko-help is not work of `#Hotstring EndChars`',
         path: 'https://www.autohotkey.com/docs/commands/_Hotstring.htm',
         // un support of EndChars
         // Hotstring("EndChars", "-()[]{}:;")
         // #Hotstring EndChars
     },
     // of EDiagDeep
-    908: { // {{{ of one Line
-        msg: 'Indentation suggestions : split to new line',
-        path: 'https://www.autohotkey.com/docs/Tutorial.htm#s84',
-        notDoc: true,
-    },
-    909: { // }}} of one Line
-        msg: 'Indentation suggestions : split to new line',
-        path: 'https://www.autohotkey.com/docs/Tutorial.htm#s84',
-        notDoc: true,
-    },
+    // 908: { // {{{ of one Line
+    //     msg: 'Indentation suggestions : split to new line',
+    //     path: 'https://www.autohotkey.com/docs/Tutorial.htm#s84',
+    //     notDoc: true,
+    // },
+    // 909: { // }}} of one Line
+    //     msg: 'Indentation suggestions : split to new line',
+    //     path: 'https://www.autohotkey.com/docs/Tutorial.htm#s84',
+    //     notDoc: true,
+    // },
 };
