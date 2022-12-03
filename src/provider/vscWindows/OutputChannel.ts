016ae35d5f99749aafaa90b86a0f791134ec99b4
CoffeeChaton <63182963+CoffeeChaton@users.noreply.github.com>
2022-12-03 12:06:23 +0800

    fix: remove `log` `output.tmLanguage.json`

--- src/provider/vscWindows/OutputChannel.ts
+++ src/provider/vscWindows/OutputChannel.ts
@@ -1,12 +1,12 @@
 import * as vscode from 'vscode';
 
-export const OutputChannel = vscode.window.createOutputChannel('AHK Neko Help', 'log');
+export const OutputChannel = vscode.window.createOutputChannel('AHK Neko Help', 'ahk');
 
 let fmtChannel: vscode.OutputChannel | null = null;
 export const OutputFormatChannel = {
     appendLine(value: string): void {
         if (fmtChannel === null) {
-            fmtChannel = vscode.window.createOutputChannel('AHK Neko Help [Format-log]', 'log');
+            fmtChannel = vscode.window.createOutputChannel('AHK Neko Help [Format-log]', 'ahk');
             fmtChannel.appendLine(`[${(new Date()).toLocaleString()}] fmtChannel start `);
         }
         fmtChannel.appendLine(value);
