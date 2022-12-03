457b9d56efe4b3d32546d7d5ebd443516f3a41bc
CoffeeChaton <63182963+CoffeeChaton@users.noreply.github.com>
2022-12-03 12:23:59 +0800

    Revert "fix: remove `log` `output.tmLanguage.json`"
    This reverts commit 016ae35d5f99749aafaa90b86a0f791134ec99b4.

--- src/provider/vscWindows/OutputChannel.ts
+++ src/provider/vscWindows/OutputChannel.ts
@@ -1,12 +1,12 @@
 import * as vscode from 'vscode';
 
-export const OutputChannel = vscode.window.createOutputChannel('AHK Neko Help', 'ahk');
+export const OutputChannel = vscode.window.createOutputChannel('AHK Neko Help', 'log');
 
 let fmtChannel: vscode.OutputChannel | null = null;
 export const OutputFormatChannel = {
     appendLine(value: string): void {
         if (fmtChannel === null) {
-            fmtChannel = vscode.window.createOutputChannel('AHK Neko Help [Format-log]', 'ahk');
+            fmtChannel = vscode.window.createOutputChannel('AHK Neko Help [Format-log]', 'log');
             fmtChannel.appendLine(`[${(new Date()).toLocaleString()}] fmtChannel start `);
         }
         fmtChannel.appendLine(value);
