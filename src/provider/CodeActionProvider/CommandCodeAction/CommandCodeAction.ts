c352688bd37f8e465f50675d1991e275a8445e0e
CoffeeChaton <63182963+CoffeeChaton@users.noreply.github.com>
2023-01-13 22:36:09 +0800

    add: misc data

---
+++ src/provider/CodeActionProvider/CommandCodeAction/CommandCodeAction.ts
@@ -0,0 +1,30 @@
+import * as vscode from 'vscode';
+import { ECommand } from '../../../command/ECommand';
+import type { TAhkFileData } from '../../../core/ProjectManager';
+import type { TAhkTokenLine } from '../../../globalEnum';
+import type { TCompletionMsgBoxParam } from './CompletionMsgBox';
+
+export function CommandCodeAction(
+    AhkFileData: TAhkFileData,
+    selection: vscode.Range | vscode.Selection,
+): vscode.Command[] {
+    if (!(selection instanceof vscode.Selection)) return [];
+    const { line, character } = selection.active;
+
+    const AhkTokenLine: TAhkTokenLine = AhkFileData.DocStrMap[line];
+    const { fistWordUp, fistWordUpCol } = AhkTokenLine;
+    if (
+        fistWordUp === 'MSGBOX'
+        && character > fistWordUpCol
+        && character < fistWordUpCol + fistWordUp.length
+    ) {
+        return [{
+            title: 'add MsgBox Options',
+            command: ECommand.CompletionMsgBox,
+            tooltip: 'by neko-help',
+            arguments: [AhkTokenLine] as TCompletionMsgBoxParam,
+        }];
+    }
+
+    return [];
+}
