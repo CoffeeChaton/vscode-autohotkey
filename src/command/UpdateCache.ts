a7e030a602df72557af78c0a1951f8fecea23af7
CoffeeChaton <63182963+CoffeeChaton@users.noreply.github.com>
2023-01-28 16:20:12 +0800

    fix: limit error scope

--- src/command/UpdateCache.ts
+++ src/command/UpdateCache.ts
@@ -20,11 +20,24 @@ export async function UpdateCacheAsync(clearCache: boolean): Promise<TAhkFileDat
     for (const uri of uriList) {
         // In the same PC, IO is relatively fast, but this decision is made for the workload of GC each time
         // and await in loop can run faster
-        // eslint-disable-next-line no-await-in-loop
-        const doc: vscode.TextDocument = await vscode.workspace.openTextDocument(uri);
-        const AhkFileData: TAhkFileData | null = pm.updateDocDef(doc);
-        if (AhkFileData !== null) {
-            FileListData.push(AhkFileData);
+        try {
+            // eslint-disable-next-line no-await-in-loop
+            const doc: vscode.TextDocument = await vscode.workspace.openTextDocument(uri);
+            const AhkFileData: TAhkFileData | null = pm.updateDocDef(doc);
+            if (AhkFileData !== null) {
+                FileListData.push(AhkFileData);
+            }
+            //
+        } catch (error: unknown) {
+            // if vscode.workspace.openTextDocument
+            // may say "The file is not displayed in the editor because it is either binary or uses an unsupported text encoding.""
+            if (error instanceof Error) {
+                log.error(error, `scan to "${uri.fsPath}" has err`);
+                log.show();
+            } else {
+                log.error('Unknown Error', `scan to "${uri.fsPath}" has err`);
+                log.show();
+            }
         }
     }
 
